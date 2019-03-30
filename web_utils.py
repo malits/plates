8e3dfd87150d05b9810ed3a277ae23398e2c61e7
malits <maxlitster@gmail.com>
2019-03-29 23:37:24 -0700

    beginning work on web interface

--- web_utils.py
+++ web_utils.py
@@ -1,1 +1,11 @@
 ## (eye emojis)
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html", id='7fMu62nb6fotKFfWhARUi4')
+
+if __name__ == '__main__':
+    app.run()
